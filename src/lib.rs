dff753d8930fdbde9f008074712353cc625fbf05
Adam Young <adamyoungy678@gmail.com>
2023-09-25 11:35:10 +0100

    feat: allowed the instantiater to set price per token

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,11 @@ blueprint! {
     struct TokenSale {
         useful_tokens_value: Vault,
         xrd_tokens_vault: Vault,
+        price_per_token: Decimal,
     }
 
     impl TokenSale {
-        pub fn new() -> ComponentAddress {
+        pub fn new(price_per_token: Decimal) -> ComponentAddress {
             let bucket: Bucket = ResourceBuilder::new_fungible()
                 .metadata("name", "Useful Tokens")
                 .metadata("symbol", "UT")
@@ -16,6 +17,7 @@ blueprint! {
             Self { 
                 useful_tokens_vault: Vault::with_bucket(bucket),
                 xrd_tokens_vault: Vault::new(RADIX_TOKEN),
+                price_per_token,
             }
             .instantiate()
             .globalize()
